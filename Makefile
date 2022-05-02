9c0ba9cb45fec159e651f35f73e8638431279c7e
Palomarrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr <88110129+Palomarrrr@users.noreply.github.com>
2022-05-01 19:41:00 -0600

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 all:
-	g++ -Os -pedantic -Wall -I/usr/X11R6/include cppwm.cpp -L/usr/X11R6/lib -lX11 -o cppwm
+	g++ -O3 -pedantic -Wall -I/usr/X11R6/include cppwm.cpp -L/usr/X11R6/lib -lX11 -o cppwm
 install:
-	g++ -Os -pedantic -Wall -I/usr/X11R6/include cppwm.cpp -L/usr/X11R6/lib -lX11 -o cppwm
+	g++ -O3 -pedantic -Wall -I/usr/X11R6/include cppwm.cpp -L/usr/X11R6/lib -lX11 -o cppwm -std=c++17
+#	g++ -O3 -pedantic -Wall -I/usr/X11R6/include stable.cpp -L/usr/X11R6/lib -lX11 -o cppwmStable -std=c++17
 	install -m 755 cppwm /usr/local/bin/cppwm
--- cppwm.cpp
+++ cppwm.cpp
@@ -1,12 +1,18 @@
-#include "common.h" // having a common header file makes things easier on my half
+#include "common.hpp" // having a common header file makes things easier on my half
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
 
 typedef struct Client
 {
 	Display *dpy; // pointer to the display the window is active on
-	Window *parent, win; //the windows parent and the window being packaged
+	Window *parent, win; //the windows parent and the window being given an id
 	unsigned int coords[2]; //0 = x, 1 = y
 	unsigned int dimentions[2]; //0 = wid, 1 = hgt
 	unsigned int borderColor[3]; //0 = r, 1 = g, 2 = b
+	bool isMini;
 	int indPrev, indNext; //the previous and next client in the list 
 } Client;
 
@@ -17,14 +23,14 @@ XWindowAttributes attr;
 XButtonEvent start;
 XEvent ev;
 static Window root;
-int scr, numClients = -1, ticker = 0, lastFocusedClient = -1;
-std::vector<Client> clients;
+int scr, ticker = 0, numMinimized = 2;
+long unsigned int lastFocusedClient = 0;
+vector<Client> clients;
 
 void die(Display *dis) // kills the WM
 {
 	if(ticker > 4)
 	{
-		std::cout << "Recieved Die CMD... Killing display server" << std::endl;
 		XCloseDisplay(dis);
 	}
 	
@@ -51,6 +57,7 @@ int init() // inits the server and its basic attributes
 	root = XDefaultRootWindow(dpy);
 	XDefineCursor(dpy, root, XCreateFontCursor(dpy, 8));
 	XSetErrorHandler(errHandle);
+	XSelectInput(dpy, root, ExposureMask|ButtonPressMask|ButtonReleaseMask|KeyPressMask|KeyReleaseMask|PointerMotionMask|SubstructureNotifyMask); // telling X to accept these types of inputs as valid
 
 	// telling X to monitor these keys/buttons for input events
 
@@ -61,28 +68,14 @@ int init() // inits the server and its basic attributes
 	XGrabButton(dpy, 4, Mod4Mask, root, True, ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
 	XGrabButton(dpy, 5, Mod4Mask, root, True, ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
 	XGrabButton(dpy, 8, Mod4Mask, root, True, ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
+	XGrabButton(dpy, 8, None, root, True, ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
 	XGrabButton(dpy, 9, None, root, True, ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);	
 
 	return 0;
 }
 
-int checkClient(Window win) // checkes what client is being passed and returns its index in the master list
-{
-	int i;
 
-	for(i = 0; i < numClients; i++)
-	{
-		if(clients[i].win == win)
-		{
-			return i;	
-		}
-	}
-	
-	return -1;
-	std::cout << "Could not find client..." << std::endl;
-}
-
-void drawBorders(int i, int thknss) // draws borders. takes in the client index and a thickness for the border;
+void drawBorders(long unsigned int i, int thknss) // draws borders. takes in the client index and a thickness for the border;
 {
 	XSetWindowBorder(dpy, clients[i].win, COLOR(clients[i].borderColor[0], clients[i].borderColor[1], clients[i].borderColor[2]));
 	XSetWindowBorderWidth(dpy, clients[i].win, thknss);	
@@ -90,7 +83,6 @@ void drawBorders(int i, int thknss) // draws borders. takes in the client index 
 
 void winToClient(Window win) // the client creator. packages the window being passed into a client
 {
-	std::cout << "Entering client creation..." << std::endl;
 
 	XWindowAttributes winAt;
 	Client tmpCli; // creates a temp client to add the attributes into
@@ -108,34 +100,45 @@ void winToClient(Window win) // the client creator. packages the window being pa
 	tmpCli.borderColor[2] = 54; //b
 	tmpCli.indPrev = -1;
 	tmpCli.indNext = -1;
+	tmpCli.isMini = false;
 
 
-	std::cout << "Adding client to the master list" << std::endl;
 	clients.push_back(tmpCli); // pushing the package into the main list
-	numClients++; // incrementing the num of clients that exist. inb4 someone says sizeof()... it doesnt want to work for some reason
-	std::cout << "Exiting client creator" << std::endl;
+	
+	drawBorders(clients.size() - 1, 1);
+
+}
+
+long unsigned int checkClient(Window win) // checkes what client is being passed and returns its index in the master list
+{
+	long unsigned int i;
+
+	for(i = 0; i < clients.size() - 1; i++)
+	{
+		if(clients[i].win == win)
+		{
+			return i;	
+		}
+	}
+	
+	winToClient(win);
+	cout << "created new client #" << clients.size() - 1 << endl;
+	return clients.size() - 1;
 }
 
 void focusWin(Window win) // changes the focused window
 {
-	int i = checkClient(win);
+	long unsigned int i = checkClient(win);
 	
-	if( i == -1 ) // checking if the client check returned that there is no client that exists and then creating a new client for it
-	{
-		std::cout << "Passed an invalid client to function \"focusWin\" on line " << __LINE__ << std::endl;
-		std::cout << "Will attempt to pass this error to the \"winToClient\" function" << std::endl;
-		winToClient(win);
-	}
-	
-	if(lastFocusedClient != i && lastFocusedClient != -1) // resetting the borders of the last focused window
+	if(lastFocusedClient != i) // resetting the borders of the last focused window
 	{
 		clients[i].indPrev = lastFocusedClient;	// setting the current client as the last focused for the next execution of this function
-		std::cout << "resetting old borders and switching lastfocus" << std::endl;
 		clients[clients[i].indPrev].borderColor[0] = 0;
 		clients[clients[i].indPrev].borderColor[1] = 43;
 		clients[clients[i].indPrev].borderColor[2] = 54;
 		clients[clients[i].indPrev].indNext = i;
 
+	//	clients[clients[i].indPrev].isFocused = false;
 
 		drawBorders(clients[i].indPrev, 1);
 	}	
@@ -144,33 +147,30 @@ void focusWin(Window win) // changes the focused window
 	clients[i].borderColor[0] = 133;
 	clients[i].borderColor[1] = 153;
 	clients[i].borderColor[2] = 0;
-	lastFocusedClient = i;
 	XSetInputFocus(dpy, clients[i].win, RevertToParent, CurrentTime);
 	XRaiseWindow(dpy, clients[i].win);
+	lastFocusedClient = i;
+	//clients[i].isFocused = true;
 
 	drawBorders(i, 1);	
 
 }
 
+void moveClientToPointer(Window win, int pointerX, int pointerY)
+{
+	long unsigned int i = checkClient(win);
+
+	XMoveWindow(dpy, clients[i].win, pointerX, pointerY);
+}
 
 void killWin(Window win) //kills the client
 {
-	int i = checkClient(win);
-
-	if( i == -1 )// checking if the client check returned that there is no client that exists and then creating a new client for it
-	{
-		std::cout << "Passed an invalid client to function \"killWin\" on line " << __LINE__ << std::endl;
-		std::cout << "Will attempt to pass this error to the \"winToClient\" function" << std::endl;
-		winToClient(win);
-	}
-	
-	std::cout << "Removing client " << i << " from the list" << std::endl;
+	long unsigned int i = checkClient(win);
 
 	XEvent evnt;
 	
 	// This was weird to figure out at first but basically it sends the current window a message to politely shut off instead of being forceful and using something like XDestroyWindow()
 
-	std::cout<< "Setting up Client Message..." << std::endl;
 	evnt.type = ClientMessage;
 	evnt.xclient.window = clients[i].win;
 	evnt.xclient.message_type = XInternAtom(dpy, "WM_PROTOCOLS", True);
@@ -178,48 +178,39 @@ void killWin(Window win) //kills the client
 	evnt.xclient.data.l[0] = XInternAtom(dpy, "WM_DELETE_WINDOW", True);	
 	evnt.xclient.data.l[1] = CurrentTime;	
 	
-	std::cout << "Event Sent!" << std::endl;
 
 	if(clients[i].indPrev != -1)
 	{
 		focusWin(clients[clients[i].indPrev].win);
+	}
+	if(clients[i].isMini)
+	{
+		numMinimized--;
 	}
 
 	XSendEvent(dpy, clients[i].win, False, NoEventMask, &evnt);
 
 	clients.erase(clients.begin() + i); // remove the client from the list
 	
-	numClients--; // setting the number of clients to reflect the actual amount in the list	
 }
+
 void restoreWin(Window win)
 {
-	int i = checkClient(win); // you should get the jist of what im doing with these by now. if not scroll back up in the code and read the explanation on previous time i use these next two functions
-	
-	if( i == -1 )
-	{
-		std::cout << "Passed an invalid client to function \"restoreWin\" on line " << __LINE__ << std::endl;
-		std::cout << "Will attempt to pass this error to the \"winToClient\" function" << std::endl;
-		winToClient(win);
-	}
+	long unsigned int i = checkClient(win); // you should get the jist of what im doing with these by now. if not scroll back up in the code and read the explanation on previous time i use these next two functions
 
 	//focuses the window and restores it to the last size saved in the clients attributes
 
 	focusWin(clients[i].win);
 	XResizeWindow(dpy, win, clients[i].dimentions[0], clients[i].dimentions[1]);
 	XMoveWindow(dpy, win, clients[i].coords[0], clients[i].coords[1]);
+	numMinimized--;
+	clients[i].isMini = false;
 }
 
 void maximizeWin(Window win)
 {
-	int i = checkClient(win); // look at the function above 	
+	long unsigned int i = checkClient(win); // look at the function above 	
 
-	if( i == -1 )
-	{
-		std::cout << "Passed an invalid client to function \"maximizeWin\" on line " << __LINE__ << std::endl;
-		std::cout << "Will attempt to pass this error to the \"winToClient\" function" << std::endl;
-		winToClient(win);
-	}
-	
 	focusWin(clients[i].win);
 	
 	//saving the unmodified dimentions of the client
@@ -231,20 +222,18 @@ void maximizeWin(Window win)
 
 	XResizeWindow(dpy, win, 1920, 1080); // this is a lazy solution that i plan on making better in the future
 	XMoveWindow(dpy, win, 0, 0);
+	
+	if(clients[i].isMini)
+	{
+		clients[i].isMini = false;		
+	}
 
 }
 
 void  minimizeWin(Window win)
 {
 	
-	int i = checkClient(win); // again look above
-	
-	if( i == -1 )
-	{
-		std::cout << "Passed an invalid client to function \"minimizeWin\" on line " << __LINE__ << std::endl;
-		std::cout << "Will attempt to pass this error to the \"winToClient\" function" << std::endl;
-		winToClient(win);
-	}
+	long unsigned int i = checkClient(win); // again look above
 
 	//saving the attributes of the current client
 
@@ -257,7 +246,9 @@ void  minimizeWin(Window win)
 
 	XLowerWindow(dpy, clients[i].win);
 	XResizeWindow(dpy, win, 15, 15);
-	XMoveWindow(dpy, win, 20 * (i + 1), (XDisplayHeight(dpy, scr) - 20)); // increments the icon along the bottom based on its position in numClients
+	XMoveWindow(dpy, win, 20 * (numMinimized), (XDisplayHeight(dpy, scr) - 20)); // increments the icon along the bottom based on its position in numClients
+	numMinimized++;	
+	clients[i].isMini = true;
 }
 
 int main(int argc, char **argv) //aaaaaand here we go the big boy function
@@ -269,8 +260,13 @@ int main(int argc, char **argv) //aaaaaand here we go the big boy function
 	while(True) // the main big boy loop to handle events and such
 	{
 		XNextEvent(dpy, &ev); // grabs next event
-			
-		if(ev.type == KeyPress) // checks whether the event is a keypress event
+
+		if(ev.type == CreateNotify) // checks if a window has just been mapped, scans the available clients to check if it
+		{
+			winToClient(ev.xcreatewindow.window);
+		//	moveClientToPointer(ev.xcreatewindow.window, ev.xbutton.x_root, ev.xbutton.y_root); // this breaking shit so i removed it for now 
+		}
+		else if(ev.type == KeyPress) // checks whether the event is a keypress event
 		{
 			if(ev.xkey.keycode == XKeysymToKeycode(dpy, XStringToKeysym("x"))) // checking whether the key pressed is the "x" key and also makes sure its being pressed with Mod4 
 			{
@@ -286,12 +282,6 @@ int main(int argc, char **argv) //aaaaaand here we go the big boy function
 			XGetWindowAttributes(dpy, start.subwindow, &attr);
 			XSetInputFocus(dpy, start.subwindow, RevertToParent, CurrentTime);
 
-			if(checkClient(start.subwindow) == -1) // checking if the selected window is in the client list and making a new one if it isnt
-			{
-				winToClient(start.subwindow);
-				std::cout << "Creating new client entry" << std::endl;
-			}
-
 			if(start.button == 2) // checks if the second (middle mouse) button was pressed with the Mod4 key
 			{
 				//kills the window
@@ -300,7 +290,6 @@ int main(int argc, char **argv) //aaaaaand here we go the big boy function
 			else if(start.button == 9) // checks whether button 9 (the front side button on my mouse) is pressed. this check does not need the Mod4 key to be pressed since i specified "None" in the init() function
 			{
 				// raises and focuses the window
-				//XSetInputFocus(dpy, start.subwindow, RevertToParent, CurrentTime);
 				focusWin(start.subwindow);
 			}
 			else if(start.button == 4) // look above ( or at the one after that for an explanation on what this is doing
@@ -319,7 +308,7 @@ int main(int argc, char **argv) //aaaaaand here we go the big boy function
 					minimizeWin(start.subwindow);	
 				}
 			}
-			else if(start.button == 8)
+			else if(start.button == 8 && start.state == Mod4Mask)
 			{
 				//checks if the window is maximized and restores to original size if true
 				if(attr.width == 1920 && attr.height == 1080)
@@ -350,6 +339,5 @@ int main(int argc, char **argv) //aaaaaand here we go the big boy function
 	}
 	
 	//nicely shutting down the server when the user wishes to shut down the WM
-	std::cout << "Attempting to close display..." << std::endl;
 	XCloseDisplay(dpy);
 }
